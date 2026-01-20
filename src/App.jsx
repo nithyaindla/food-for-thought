1c73b37126ab2e8934b99b7a5f8b9bb9d5a57aa4
Nithya Indlamuri <nithi@unc.edu>
2026-01-20 12:17:30 -0500

    titlefi65

--- src/App.jsx
+++ src/App.jsx
@@ -419,7 +419,7 @@ function App() {
         </div>
         
         {/* Title moved to Bottom Right */}
-        <div className="absolute -bottom-10 right-6 md:right-12 relative z-10 text-left">
+        <div className="absolute -bottom-15 right-6 md:right-12 relative z-10 text-left">
           <h1 className="text-[8vw] leading-[0.85] font-syne font-black tracking-tighter uppercase text-white mix-blend-overlay opacity-90">
             TONY'S<br/>KITCHEN<br/>LAB
           </h1>
